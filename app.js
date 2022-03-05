49939dcb3d0da8cdbfc995381a2f2ba764b5cc84
Anshul Shende <anshulshende40@gmail.com>
2022-03-05 18:35:34 +0530

    Added Mongo db config and connected to Mongo db atlas using Moongose

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const cookieParser = require('cookie-parser');
 var path = require('path');
+var cors = require('cors');
+const db = require('./config/dbconfig');
+require('dotenv').config();
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -7,10 +11,21 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+db.once('open', ()=>{
+    console.log('Connected to Mongo DB Atlas');
+});
+
+db.on('error', (err) => {
+    console.log(err);
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
